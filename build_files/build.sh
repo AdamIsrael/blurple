4c6ddcc4c2c174f34d2a133ff95546ae97130380
Adam Israel <adam@adamisrael.com>
2025-07-16 11:31:12 -0400

    Add glow

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,8 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux the_silver_searcher ripgrep
-
+dnf5 install -y tmux the_silver_searcher ripgrep glow
 
 # hyprland
 dnf5 install -y hyprland
