ee1472cd85001a5a1141dda3e8a16bc1b1d2ebb7
Adam Israel <adam@adamisrael.com>
2025-10-20 14:01:48 -0400

    Add 1password + cli into the base image
    Borrowed a script from bsherman to install 1password into the base
    image, so I won't have to layer them.

--- build_files/build.sh
+++ build_files/build.sh
@@ -56,16 +56,7 @@ dnf -y copr disable dejan/lazygit
 #systemctl enable tailscaled
 
 # 1Password
-curl https://downloads.1password.com/linux/keys/1password.asc -o /etc/pki/rpm-gpg/1password.asc
-cat > /etc/yum.repos.d/1password.repo << EOF
-[1password]
-name=1Password Stable Channel
-baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/1password.asc
-EOF
+/ctx/desktop-1password.sh
 
 # Installing 1Password fails with:
 #
